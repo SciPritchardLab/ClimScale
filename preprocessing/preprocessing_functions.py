9e3910e123f6f5b304a22213ae720db0107f8507
Jerry Lin <jerrylin247365@gmail.com>
2022-09-15 15:15:31 -0700

    renamed inpsub to inp_sub and inpdiv to inp_div for consistency

--- preprocessing/preprocessing_functions.py
+++ preprocessing/preprocessing_functions.py
@@ -218,43 +218,43 @@ def normalize_input_train(X_train, reshaped = True, norm = "standard", save_file
         train_max = X_train.max(axis = (1,2,3))[:, np.newaxis]
         
     if norm == "standard":
-        inpsub = train_mu
-        inpdiv = train_std
+        inp_sub = train_mu
+        inp_div = train_std
         
     elif norm == "range":
-        inpsub = train_min
-        inpdiv = train_max - train_min
+        inp_sub = train_min
+        inp_div = train_max - train_min
         
     #normalizing
-    X_train = ((X_train - inpsub)/inpdiv).transpose()
+    X_train = ((X_train - inp_sub)/inp_div).transpose()
     #normalized
     
     print("X_train shape: ")
     print(X_train.shape)
     print("INP_SUB shape: ")
-    print(inpsub.shape)
+    print(inp_sub.shape)
     print("INP_DIV shape: ")
-    print(inpdiv.shape)
+    print(inp_div.shape)
     
     if save_files:
         with open(save_path + "trainInput.npy", 'wb') as f:
             np.save(f, np.float32(X_train))
-        np.savetxt(norm_path + "inp_sub.txt", inpsub, delimiter=',')
-        np.savetxt(norm_path + "inp_div.txt", inpdiv, delimiter=',')
+        np.savetxt(norm_path + "inp_sub.txt", inp_sub, delimiter=',')
+        np.savetxt(norm_path + "inp_div.txt", inp_div, delimiter=',')
         return
     
-    return X_train, inpsub, inpdiv
+    return X_train, inp_sub, inp_div
 
 
-def normalize_input_val(X_val, inpsub, inpdiv, save_files = False, save_path = "../training/training_data/"):
+def normalize_input_val(X_val, inp_sub, inp_div, save_files = False, save_path = "../training/training_data/"):
     #normalizing
-    X_val = ((X_val - inpsub)/inpdiv).transpose()
+    X_val = ((X_val - inp_sub)/inp_div).transpose()
     print("X_val shape: ")
     print(X_val.shape)
     print("INP_SUB shape: ")
-    print(inpsub.shape)
+    print(inp_sub.shape)
     print("INP_DIV shape: ")
-    print(inpdiv.shape)
+    print(inp_div.shape)
     
     if save_files:
         with open(save_path + "valInput.npy", 'wb') as f:
