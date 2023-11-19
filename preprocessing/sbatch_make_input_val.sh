227d87b690a7d218aee702c89df5d425f6c4276a
Jerry Lin <jlin96@br011.ib.bridges2.psc.edu>
2023-11-18 23:32:17 -0500

    adding missing preprocessing files

---
+++ preprocessing/sbatch_make_input_val.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name="make_input_val"
+#SBATCH --output="make_input_val.out"
+#SBATCH --partition=RM-512
+#SBATCH --export=ALL
+#SBATCH --account=atm200007p
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jerryl9@uci.edu
+#SBATCH -t 4:20:00
+
+source /opt/packages/anaconda3/etc/profile.d/conda.sh
+module load anaconda3
+conda activate preprocessing
+python make_input_val.py
