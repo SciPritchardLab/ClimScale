41ea7bb0ec54ac63149b0ab56b657f4ddcfc7c86
Jerry Lin <jlin96@br012.ib.bridges2.psc.edu>
2024-02-27 15:50:18 -0500

    reduced time alotted for sbatch preprocessing script

--- preprocessing/sbatch_preprocess_data.sh
+++ preprocessing/sbatch_preprocess_data.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=atm200007p
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jerryl9@uci.edu
-#SBATCH -t 4:15:00
+#SBATCH -t 1:45:00
 
 source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load anaconda3
