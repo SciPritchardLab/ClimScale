f1bb6f9b54aa19f5850c8ce5dc9aa3f2e8ca7991
Jerry Lin <jlin96@br013.ib.bridges2.psc.edu>
2024-03-05 12:35:36 -0500

    expanded training data and fixed prepreprocessing bug

--- preprocessing/sbatch_preprocess_data.sh
+++ preprocessing/sbatch_preprocess_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name="preprocess_data_kitchensinkmse"
+#SBATCH --job-name="preprocess_data_alphaone"
 #SBATCH --output="preprocess_data.out"
 #SBATCH --partition=RM-512
 #SBATCH --export=ALL
 #SBATCH --account=atm200007p
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jerryl9@uci.edu
-#SBATCH -t 1:45:00
+#SBATCH -t 2:45:00
 
 source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load anaconda3
