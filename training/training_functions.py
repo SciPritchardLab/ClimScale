e37c202b7dd66be25b7f0752716dcf9e421e262f
Jerry Lin <jlin96@br013.ib.bridges2.psc.edu>
2024-03-11 23:04:32 -0400

    QHAdam doesnt work with learning rate scheduler

--- training/training_functions.py
+++ training/training_functions.py
@@ -5,7 +5,6 @@ from keras.layers import BatchNormalization
 from keras.layers import LeakyReLU
 from keras.layers import Dropout
 import tensorflow_addons as tfa
-from qhoptim.tf import QHAdamOptimizer
 from tensorflow.keras.callbacks import LearningRateScheduler
 import os
 
@@ -28,13 +27,11 @@ def build_model(hp):
         model.add(Dropout(dp_rate))
     model.add(Dense(60, kernel_initializer='normal', activation='linear'))
     initial_learning_rate = hp.Float("lr", min_value=1e-6, max_value=1e-3, sampling="log")
-    optimizer = hp.Choice("optimizer", ["adam", "RAdam", "QHAdam"])
+    optimizer = hp.Choice("optimizer", ["adam", "RAdam"])
     if optimizer == "adam":
         optimizer = keras.optimizers.Adam(learning_rate = initial_learning_rate)
     elif optimizer == "RAdam":
         optimizer = tfa.optimizers.RectifiedAdam(learning_rate = initial_learning_rate)
-    elif optimizer == "QHAdam":
-        optimizer = QHAdamOptimizer(learning_rate = initial_learning_rate, nu2=1.0, beta1=0.995, beta2=0.999)
     model.compile(optimizer = optimizer, loss = 'mse', metrics = ["mse"])
     return model
 
