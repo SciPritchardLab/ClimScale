e1a2a3991b53629f104817eee65776542dff317b
Jerry Lin <jlin96@br011.ib.bridges2.psc.edu>
2024-03-19 20:41:48 -0400

    renamed preprocessing job

--- preprocessing/sbatch_preprocess_data.sh
+++ preprocessing/sbatch_preprocess_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="preprocess_data_stupidsimple"
+#SBATCH --job-name="preprocess_data_hypersampling"
 #SBATCH --output="preprocess_data.out"
 #SBATCH --partition=RM-512
 #SBATCH --export=ALL
