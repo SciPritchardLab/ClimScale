08d0b4c87c69b654fe1ecfab1b368eaa4c08cb7d
Jerry Lin <jlin96@br014.ib.bridges2.psc.edu>
2024-04-14 21:37:10 -0400

    edited animation script for totals

--- coupling_folder/make_animations.py
+++ coupling_folder/make_animations.py
@@ -118,7 +118,81 @@ def plot_diff(xr_data, var, save_files = True):
     else:
         return latlev_clip, worldmap_clip
 
+def plot_diff_sum(xr_data, var, save_files = True):
+    num_timesteps = len(xr_data['time'])
+    if var == 'NNTBSP':
+        map_cmap = plb.cm.afmhot
+        cmap = 'bwr'
+        vmin = -5
+        vmax = 5
+    elif var == 'NNQBSP':
+        map_cmap = plb.cm.bone
+        cmap = 'BrBG'
+        vmin = -.0015
+        vmax = .0015
+    elif var == 'heating':
+        map_cmap = plb.cm.afmhot
+        cmap = 'bwr'
+        vmin = -7e-3
+        vmax = 7e-3
+    elif var == 'moistening':
+        map_cmap = plb.cm.bone
+        cmap = 'BrBG'
+        vmin = -5e-6
+        vmax = 5e-6
+    elif var == 'TTEND':
+        map_cmap = plb.cm.bone
+        cmap = 'BrBG'
+        vmin = -1
+        vmax = 1
+    map_cmaplist = [map_cmap(i) for i in range(map_cmap.N)]
+    map_cmap = mpl.colors.LinearSegmentedColormap.from_list('Custom cmap', map_cmaplist, map_cmap.N)
+    bounds = np.linspace(vmin, vmax, 11)
+    norm = mpl.colors.BoundaryNorm(bounds, map_cmap.N)
+
+    latlev_image_files = []
+    worldmap_image_files = []
+    if os.path.exists(var):
+        shutil.rmtree(var)
+    os.makedirs(var, exist_ok=True)
+    os.makedirs(var + '_cum/latlev/', exist_ok = True)
+    os.makedirs(var + '_cum/worldmap/', exist_ok = True)
+    var_diff = (xr_data[var] - sp_data[var])
+    map_var_diff = np.sum(var_diff*pressure_weighting[:num_timesteps,...], axis = 1)
+    for t in range(0, num_timesteps):
+        day = int(t/48)
+        if t%48 == 0:
+            var_diff.isel(time = slice(0, t)).sum(dim = 'time').mean(dim = 'lon').plot(x='lat', y='lev', yincrease=False, cmap = cmap, vmin = vmin, vmax = vmax)
+            latlev_image_file = var + "_cum/latlev/" + var + str(t).zfill(3) + ".png"
+            title_str = var + ' cumulative difference on day ' + str(day)
+            plt.title(title_str)
+            plt.savefig(latlev_image_file)
+            latlev_image_files.append(latlev_image_file)
+            plt.clf()
+            plt.close('all')
+            fig, ax = plt.subplots(1,1,subplot_kw={'projection':ccrs.Robinson(central_longitude=180)},
+                            figsize=(15,6))
+            ax.gridlines()
+            ax.set_title(title_str)
+            imag = ax.pcolormesh(lons, lats, map_var_diff[t,:,:], transform=ccrs.PlateCarree(), cmap=cmap,norm=norm)
+            cbar_ax = fig.add_axes([0.84, 0.12, 0.02, 0.76])
+            fig.colorbar(imag, cax = cbar_ax) 
+            worldmap_image_file = var + "_cum/worldmap/" + var + str(t).zfill(3) + ".png"
+            plt.savefig(worldmap_image_file)
+            worldmap_image_files.append(worldmap_image_file)
+            plt.clf()
+            plt.close('all')
+    latlev_clip = ImageSequenceClip(sorted(latlev_image_files), fps = fps)
+    worldmap_clip = ImageSequenceClip(sorted(worldmap_image_files), fps = fps)
+    if save_files:
+        latlev_clip.write_videofile(var + '_cum/latlev/' + var + '_latlev.mp4')
+        worldmap_clip.write_videofile(var + '_cum/worldmap/' + var + '_worldmap.mp4')
+    else:
+        return latlev_clip, worldmap_clip
+
 plot_diff(nn_data, 'heating')
 plot_diff(nn_data, 'moistening')
+plot_diff_sum(nn_data, 'heating')
+plot_diff_sum(nn_data, 'moistening')
 plot_diff(nn_data, 'NNTBSP')
 plot_diff(nn_data, 'NNQBSP')
