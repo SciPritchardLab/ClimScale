eab323237c4196b881d1c21c1ade389ab7c048b1
Jerry Lin <jlin96@br014.ib.bridges2.psc.edu>
2024-04-25 15:01:09 -0400

    pushing bug fix for reverse reshape

--- preprocessing/preprocessing_functions.py
+++ preprocessing/preprocessing_functions.py
@@ -130,7 +130,7 @@ def reverse_reshape(reshaped_arr, original_shape):
     '''
     reshaped_arr should be num_samples x features for this function to work properly
     '''
-    arr = reshaped_arr.transpose().reshape(original_shape[1], original_shape[0], original_shape[2], original_shape[3], order='F')
+    arr = reshaped_arr.transpose().reshape(60, original_shape[0], original_shape[2], original_shape[3], order='F')
     ans = arr.transpose(1,0,2,3)
     print(ans.shape)
     return ans
