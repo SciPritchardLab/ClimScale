0ebcf1a37e8f1f3dbdc2ac01bf9b53f74ea93967
Jerry Lin <jlin96@r331.ib.bridges2.psc.edu>
2024-05-03 12:55:52 -0400

    combined two scripts for efficiency purposes

--- coupling_folder/plot_online_diff.py
+++ coupling_folder/plot_online_diff.py
@@ -190,3 +190,13 @@ with open('prognostic_T.pkl', 'wb') as f:
 
 with open('prognostic_Q.pkl', 'wb') as f:
     pickle.dump(diff_Q, f)
+
+model_info = pd.read_pickle('RESULTS_' + config_subdir + '_sorted.pandas.pkl')
+model_info['offline_heating'] = pd.Series(offline_test_error[:,0], name = 'offline_heating', index = model_info.index)
+model_info['offline_moistening'] = pd.Series(offline_test_error[:,1], name = 'offline_moistening', index = model_info.index)
+model_info['num_months'] = pd.Series([len(prognostic_T[x]) for x in model_info.index], name = 'num_months', index = model_info.index)
+assert model_info['num_months'].equals(pd.Series([len(prognostic_Q[x]) for x in model_info.index], name = 'num_months', index = model_info.index))
+model_info['online_temperature'] = pd.Series([np.mean(prognostic_T[x]) if len(prognostic_T[x])==12 else None for x in model_info.index], name = 'prognostic_T', index = model_info.index)
+model_info['online_moisture'] = pd.Series([np.mean(prognostic_Q[x])*1000 if len(prognostic_Q[x])==12 else None for x in model_info.index], name = 'prognostic_Q', index = model_info.index)
+
+model_info.to_pickle(config_subdir + '_df.pandas.pkl')
