2d0d2e96cd524a266e8de89ae0243996dec28a10
Jerry Lin <jlin96@br011.ib.bridges2.psc.edu>
2024-11-09 12:03:51 -0500

    added misisng folders and adjusted job submission time to reflect efficiency increase

--- offline_evaluation/sbatch_get_offline_error.sh
+++ offline_evaluation/sbatch_get_offline_error.sh
@@ -9,7 +9,7 @@
 #SBATCH --account=atm200007p
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jerryl9@uci.edu
-#SBATCH -t 24:00:00
+#SBATCH -t 8:00:00
 
 source /opt/packages/anaconda3/etc/profile.d/conda.sh
 module load anaconda3
